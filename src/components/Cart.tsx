834091023bafcd70458a2bda3e20c33de7ac84f4
Михаил <misha1234567890m@gmail.com>
2022-09-21 14:09:52 +0300

    Закончил верстку StartPage

--- src/components/Cart.tsx
+++ src/components/Cart.tsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import {ICart} from './Page/StartPage'
+import style from './Cart.module.scss'
 
 const Cart :React.FC<ICart>= ({imgUrl,id,text,title}) => {
     return (
-        <div>
-            <img height={40 } src={imgUrl} alt=""/>
+        <div className={style.cart}>
+            <img height={115 } src={imgUrl} alt=""/>
             <h4>{title}</h4>
             <p>{text}</p>
 
