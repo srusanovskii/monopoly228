f3b0e91cae4938a799b47bd8881e625e08a3f471
Михаил <misha1234567890m@gmail.com>
2022-09-22 11:18:48 +0300

    Rework after review

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import Header from "./components/Header/Header";
+import style from './App.scss'
+import StartPage from "./components/Page/StartPage";
+
+
+function App() {
+  return (
+
+
+ <div className={style.App}>
+     <Header/>
+    <StartPage/>
+
+
+ </div>
+  );
+}
+
+export default App;
